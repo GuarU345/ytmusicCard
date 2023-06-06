bd74eb3dfcf0afb394c7635f0d0eeeed61f125f7
abel34 <abel.dominguez@sw-argos.com>
2023-06-06 12:59:41 -0600

    initial repo

---
+++ index.js
@@ -0,0 +1,9 @@
+const TITLE_SONG = "LOVE"
+const AUTHOR_SONG = "Kendrick Lamar"
+
+let charge_title = document.getElementById("title-song").innerHTML = TITLE_SONG
+let charge_author = document.getElementById('author-song').innerHTML = AUTHOR_SONG
+
+document.addEventListener("DOMContentLoaded", function(event) {
+   charge_title
+});
